d3a5064f96affdf2fb521364f87b10923396a094
suhelkhanca <suhel.hata@gmail.com>
2024-12-17 22:19:18 +0530

    add footer

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+        <div class="container">
+            <div class="university-name">
+                <h5>Suhel Khan © 2024 Copyright: Aligarh Muslim University</h5>
+            </div>
+        </div>
+</footer>
