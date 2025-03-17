56a0ea49b18c85ce6a05e80d5a96e27640f6c2a5
suhelkhanca <suhel.hata@gmail.com>
2025-03-18 00:50:12 +0530

    refactor

--- completely_delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include"./dbconnect.php";
-$username=$_GET['id'];
-$sql="DELETE FROM `normaluser` WHERE `normaluser`.`username` = '$username'";
-$result=mysqli_query($connection,$sql);
-header("location:./removeUser.php");
-?>
